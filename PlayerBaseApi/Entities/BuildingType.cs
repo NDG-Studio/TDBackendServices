a95dd74c74a67fe07f35e6d61c74dd347e0600fb
ugurcanbagriyanik <ugurcan.bagriyanik@gmail.com>
2022-09-12 12:12:10 +0300

    [playerbaseapi] buildurl added

--- PlayerBaseApi/Entities/BuildingType.cs
+++ PlayerBaseApi/Entities/BuildingType.cs
@@ -7,6 +7,7 @@ namespace PlayerBaseApi.Entities
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string BuildUrl { get; set; }
         public int MaxLevel { get; set; }
         public bool IsActive { get; set; }
     }
--- PlayerBaseApi/PlayerBaseContext.cs
+++ PlayerBaseApi/PlayerBaseContext.cs
@@ -22,15 +22,15 @@ namespace PlayerBaseApi
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<BuildingType>().HasData(
-                new BuildingType() { Id = 1, Name = "Base", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 2, Name = "Gang Tower", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 3, Name = "Wall", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 4, Name = "Hospital", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 5, Name = "Prison", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 6, Name = "Market", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 7, Name = "Altar", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 8, Name = "Watch Tower", MaxLevel=1000, IsActive = true },
-                new BuildingType() { Id = 9, Name = "Research Laboratory", MaxLevel=1000, IsActive = true }
+                new BuildingType() { Id = 1, Name = "Base", MaxLevel=1000, BuildUrl= "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-5.png", IsActive = true },
+                new BuildingType() { Id = 2, Name = "Gang Tower", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 3, Name = "Wall", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 4, Name = "Hospital", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 5, Name = "Prison", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 6, Name = "Market", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-6.png", IsActive = true },
+                new BuildingType() { Id = 7, Name = "Altar", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 8, Name = "Watch Tower", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 9, Name = "Research Laboratory", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true }
             );
             modelBuilder.Entity<PlayerBasePlacement>().HasData(
                 new PlayerBasePlacement() { Id = 1,BuildingTypeId=1,BuildingLevel=1,CoordX=1,CoordY=1,UpdateEndDate=null,UserId=1 }
