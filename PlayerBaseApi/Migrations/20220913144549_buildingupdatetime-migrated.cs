d72e9557f1b19e52b6757db469db8117925a27fb
ugurcanbagriyanik <ugurcan.bagriyanik@gmail.com>
2022-09-13 17:56:48 +0300

    [playerbaseapi] buildingupdatetime dummy data inserted

---
+++ PlayerBaseApi/Migrations/20220913144549_buildingupdatetime-migrated.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerBaseApi.Migrations
+{
+    public partial class buildingupdatetimemigrated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "BuildingUpdateTime",
+                columns: new[] { "Id", "BuildingTypeId", "Level", "UpdateDuration" },
+                values: new object[,]
+                {
+                    { 1, 1, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 2, 2, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 3, 3, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 4, 4, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 5, 5, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 6, 6, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 7, 7, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 8, 8, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 9, 9, 2, new TimeSpan(0, 0, 2, 0, 0) },
+                    { 10, 1, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 11, 2, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 12, 3, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 13, 4, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 14, 5, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 15, 6, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 16, 7, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 17, 8, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 18, 9, 3, new TimeSpan(0, 0, 5, 0, 0) },
+                    { 19, 1, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 20, 2, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 21, 3, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 22, 4, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 23, 5, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 24, 6, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 25, 7, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 26, 8, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 27, 9, 4, new TimeSpan(0, 0, 10, 0, 0) },
+                    { 28, 1, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 29, 2, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 30, 3, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 31, 4, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 32, 5, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 33, 6, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 34, 7, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 35, 8, 5, new TimeSpan(0, 0, 30, 0, 0) },
+                    { 36, 9, 5, new TimeSpan(0, 0, 30, 0, 0) }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 33);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 34);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 35);
+
+            migrationBuilder.DeleteData(
+                table: "BuildingUpdateTime",
+                keyColumn: "Id",
+                keyValue: 36);
+        }
+    }
+}
--- PlayerBaseApi/PlayerBaseContext.cs
+++ PlayerBaseApi/PlayerBaseContext.cs
@@ -24,18 +24,57 @@ namespace PlayerBaseApi
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<BuildingType>().HasData(
-                new BuildingType() { Id = 1, Name = "Base", MaxLevel=1000, BuildUrl= "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-5.png", IsActive = true },
-                new BuildingType() { Id = 2, Name = "Gang Tower", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 3, Name = "Wall", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 4, Name = "Hospital", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 5, Name = "Prison", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 6, Name = "Market", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-6.png", IsActive = true },
-                new BuildingType() { Id = 7, Name = "Altar", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 8, Name = "Watch Tower", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
-                new BuildingType() { Id = 9, Name = "Research Laboratory", MaxLevel=1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true }
+                new BuildingType() { Id = 1, Name = "Base", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-5.png", IsActive = true },
+                new BuildingType() { Id = 2, Name = "Gang Tower", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 3, Name = "Wall", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 4, Name = "Hospital", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 5, Name = "Prison", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 6, Name = "Market", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-6.png", IsActive = true },
+                new BuildingType() { Id = 7, Name = "Altar", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 8, Name = "Watch Tower", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true },
+                new BuildingType() { Id = 9, Name = "Research Laboratory", MaxLevel = 1000, BuildUrl = "https://gaming.ndgstudio.com.tr/wp-content/uploads/2021/09/h1-client-img-4.png", IsActive = true }
             );
             modelBuilder.Entity<PlayerBasePlacement>().HasData(
-                new PlayerBasePlacement() { Id = 1,BuildingTypeId=1,BuildingLevel=1,CoordX=1,CoordY=1,UpdateEndDate=null,UserId=1 }
+                new PlayerBasePlacement() { Id = 1, BuildingTypeId = 1, BuildingLevel = 1, CoordX = 1, CoordY = 1, UpdateEndDate = null, UserId = 1 }
+            );
+
+            modelBuilder.Entity<BuildingUpdateTime>().HasData(
+                new BuildingUpdateTime() { Id = 1, BuildingTypeId = 1, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 2, BuildingTypeId = 2, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 3, BuildingTypeId = 3, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 4, BuildingTypeId = 4, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 5, BuildingTypeId = 5, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 6, BuildingTypeId = 6, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 7, BuildingTypeId = 7, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 8, BuildingTypeId = 8, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 9, BuildingTypeId = 9, Level = 2, UpdateDuration = new TimeSpan(0, 2, 0) },
+                new BuildingUpdateTime() { Id = 10, BuildingTypeId = 1, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 11, BuildingTypeId = 2, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 12, BuildingTypeId = 3, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 13, BuildingTypeId = 4, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 14, BuildingTypeId = 5, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 15, BuildingTypeId = 6, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 16, BuildingTypeId = 7, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 17, BuildingTypeId = 8, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 18, BuildingTypeId = 9, Level = 3, UpdateDuration = new TimeSpan(0, 5, 0) },
+                new BuildingUpdateTime() { Id = 19, BuildingTypeId = 1, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 20, BuildingTypeId = 2, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 21, BuildingTypeId = 3, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 22, BuildingTypeId = 4, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 23, BuildingTypeId = 5, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 24, BuildingTypeId = 6, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 25, BuildingTypeId = 7, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 26, BuildingTypeId = 8, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 27, BuildingTypeId = 9, Level = 4, UpdateDuration = new TimeSpan(0, 10, 0) },
+                new BuildingUpdateTime() { Id = 28, BuildingTypeId = 1, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 29, BuildingTypeId = 2, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 30, BuildingTypeId = 3, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 31, BuildingTypeId = 4, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 32, BuildingTypeId = 5, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 33, BuildingTypeId = 6, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 34, BuildingTypeId = 7, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 35, BuildingTypeId = 8, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) },
+                new BuildingUpdateTime() { Id = 36, BuildingTypeId = 9, Level = 5, UpdateDuration = new TimeSpan(0, 30, 0) }
             );
 
         }
