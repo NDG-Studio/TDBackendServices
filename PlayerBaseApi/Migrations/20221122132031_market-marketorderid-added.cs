ff84152d790b924904c22653ac0ff94d25ab94e7
ugurcanbagriyanik <ugurcan.bagriyanik@gmail.com>
2022-11-22 16:29:22 +0300

    [playerbase] marketorderid migration

---
+++ PlayerBaseApi/Migrations/20221122132031_market-marketorderid-added.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerBaseApi.Migrations
+{
+    public partial class marketmarketorderidadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MarketOrderId",
+                table: "MarketItem",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarketOrderId",
+                table: "MarketItem");
+        }
+    }
+}
