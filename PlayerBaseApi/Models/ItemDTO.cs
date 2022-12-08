0e0c98a6a0a70ec4f279a3d4d218dd1f865aecee
ugurcanbagriyanik <ugurcan.bagriyanik@gmail.com>
2022-12-08 19:38:56 +0300

    [playerbase] itemDTO value props added

--- PlayerBaseApi/Models/ItemDTO.cs
+++ PlayerBaseApi/Models/ItemDTO.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public int? Value1 { get; set; }
+        public int? Value2 { get; set; }
         public string? ItemTypeName { get; set; }
         public bool? IsConsumable { get; set; }
     }
